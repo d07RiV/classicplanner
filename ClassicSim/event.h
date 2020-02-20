b6a50d99eacf31c5962f7a8ba476900d2cb99efb
Andrey Kolosov <andrey.kolosov@glidewelldental.com>
2020-02-20 19:21:19 +0300

    change

--- ClassicSim/event.h
+++ ClassicSim/event.h
@@ -1,18 +1,23 @@
 #pragma once
 
+// Base event class
+// Every event instance must be scheduled in the derived constructor by calling schedule() or the appropriate Event constructor
+// Event instance will be destroyed after the event is executed or canceled and the pointer might be recycled for future use
+// As such, events are referenced using EventHandle's from the outside, which checks for event validity before 
 class Event : private noncopyable
 {
 public:
     Event( Sim& sim );
     Event( Sim& sim, timespan_t delta ) : Event( sim )
     {
-        schedule( delta );
+        schedule_( delta );
     }
 
     virtual ~Event() {}
 
-    void schedule( timespan_t delta );
     void reschedule( timespan_t delta );
+    // this will delete the event, invalidating the handle!
+    void cancel();
 
     virtual void execute() = 0;
     virtual const char* name() const
@@ -20,39 +25,110 @@ public:
         return "Event";
     }
 
-protected:
-    Sim& sim_;
-
-    static uint64_t uid_counter_;
-    uint64_t uid_;
-    timespan_t time_;
-    int scheduled_ = -1;
-
-    template<typename Event, typename... Args>
-    friend Event* make_event( Sim& sim, Args&... args );
-
-    static void* operator new( std::size_t size, Sim& sim )
+    uint64_t uid() const
     {
-        return sim.events.allocate_event( size );
+        return uid_;
     }
+
+    Sim& sim() const
+    {
+        return sim_;
+    }
+
+    timespan_t time() const
+    {
+        return time_;
+    }
+
+    bool scheduled() const
+    {
+        return scheduled_ != std::numeric_limits<size_t>::max();
+    }
+
+private:
+    Sim& sim_;
+    static uint64_t uidCounter_;
+    uint64_t uid_;
+
+    friend class EventManager;
+    size_t scheduled_ = std::numeric_limits<size_t>::max();
+    timespan_t time_;
+
+    template<typename T, typename... Args>
+    friend EventHandle makeEvent( Sim& sim, Args&... args );
+
+    static void* operator new( std::size_t size, Sim& sim );
+    // placement delete is only called when an exception occurs in event constructor, so it's okay to have a bit of a leak here
+    static void operator delete( void*, Sim& sim ) {}
+    static void operator delete( void* ) {}
+    static void* operator new( std::size_t ) = delete;
+
+protected:
+    void schedule_( timespan_t delta );
 };
 
-template<class T = Event>
+template<typename T, typename... Args>
+inline EventHandle makeEvent( Sim& sim, Args&... args )
+{
+    static_assert( std::is_base_of<Event, T>::value, "Event type not derived from Event" );
+    auto ptr = new( sim ) T( sim, std::forward<Args>( args )... );
+    assert( ptr->scheduled() );
+    return ptr;
+}
+
 class EventHandle
 {
 public:
+    EventHandle( Event* event ) :
+        event_( event ),
+        uid_( event->uid() )
+    {
+    }
+
+    explicit operator bool() const
+    {
+        return event_->uid() == uid_;
+    }
+
+    Event* operator->() const
+    {
+        assert( event_->uid() == uid_ );
+        return event_;
+    }
 
 private:
     Event* event_;
+    uint64_t uid_;
 };
 
 class EventManager
 {
 public:
-    void* allocate_event( size_t size );
+    timespan_t time() const
+    {
+        return time_;
+    }
+    bool empty() const
+    {
+        return heap_.empty();
+    }
+
+    void scheduleEvent( Event* event, timespan_t time );
+    void rescheduleEvent( Event* event, timespan_t time );
+    void cancelEvent( Event* event );
+
+    void* allocateEvent( size_t size );
+
+    void execute();
 
 private:
-    Sim& sim_;
+    void recycleEvent_( Event* event );
+
+    timespan_t time_;
 
     std::vector<Event*> heap_;
+    std::vector<void*> recycledEvents_;
+
+    size_t siftUp_( size_t pos );
+    size_t siftDown_( size_t pos );
 };
--- ClassicSim/sim.h
+++ ClassicSim/sim.h
@@ -2,6 +2,12 @@
 
 #include <cstdint>
 #include <chrono>
+#include <cassert>
+
+#include <memory>
+#include <vector>
+
+#include "util.h"
 
 using timespan_t = std::chrono::duration<int64_t>;
 
@@ -29,10 +35,46 @@ class EventManager;
 class Event;
 class EventHandle;
 
+#include "event.h"
+
 class Sim
 {
 public:
-    EventManager events;
+    timespan_t time() const
+    {
+        return eventManager.time();
+    }
+
+    EventManager eventManager;
+
+    template<typename T>
+    EventHandle after( timespan_t delta, const T& fn );
 };
 
-#include "event.h"
+template<typename T>
+EventHandle Sim::after( timespan_t delta, const T& fn )
+{
+    class FunctionEvent : public Event
+    {
+    public:
+        FunctionEvent( Sim& sim, timespan_t delta, const T& fn ) :
+            Event( sim, delta ),
+            fn_( fn )
+        {
+        }
+
+        const char* name() const override
+        {
+            return "FunctionEvent";
+        }
+
+        void execute() override
+        {
+            fn_();
+        }
+    private:
+        T fn_;
+    };
+
+    return makeEvent( *this, delta, fn );
+}
